69e30e38104f6002f8a4cbd3995596d32e64e56f
TomIshiyama <honngannji@gmail.com>
2025-05-15 19:58:36 +0900

    chore: add env files

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -3,3 +3,9 @@ export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET ?? "";
 export const SLACK_APP_TOKEN = process.env.SLACK_APP_TOKEN ?? "";
 
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3456;
+
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "";
+export const WHISPER_LOCAL_ENDPOINT = process.env.WHISPER_LOCAL_ENDPOINT ?? "";
+export const WHISPER_LOCAL_INSTANCE = process.env.WHISPER_LOCAL_INSTANCE
+  ? process.env.WHISPER_LOCAL_INSTANCE === "true"
+  : false;
